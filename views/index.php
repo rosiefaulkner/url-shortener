c4a42582adcd96eacc3cf136363baae57839de36
Rosie Faulkner <rosiefaulkner@0069L2Z7CRMLCVF.attlocal.net>
2020-11-08 21:41:47 -0500

    initial files

---
+++ views/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>URL Shortner</title>
+</head>
+<body>
+<h1>URL Shortener</h1>
+<form method="post" action="/index.php">
+    <label for="url">URL</label>
+    <input type="text" name="url" id="url">
+    <input type="submit" name="shorten" id="shorten" value="shorten">
+</form>
+</body>
+</html>
